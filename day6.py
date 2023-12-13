51a477274028200cbd3344b533b32040d768b034
Chris Wahl <christopher.g.wahl@gmail.com>
2023-12-12 20:44:04 -0500

    day 6

--- day6.py
+++ day6.py
@@ -1,28 +1,32 @@
 def countWays(time, dist):
     count = 0
     for i in range(time):
-        if (dist//(time-i)) > i:
+        if i > (dist//(time-i)):
             count = count + 1
-
     return count
 
 
-with open("6_test.txt") as file:
+with open("6.txt") as file:
     times = file.readline()
     times = times.split(":")
-    times = times[1][1:-1]
-    times = times.strip().split(" ")
-    times = times.remove(" ")
+    times = times[1][:-1]
+    times = times.strip()#.split(" ")
+
     dist = file.readline()
     dist = dist.split(":")
-    dist = dist[1][1:-1]
-    dist = dist.strip().split(" ")
-    dist = dist.remove(" ")
+    dist = dist[1][:-1]
+    dist = dist.strip()#.split(" ")
+
+    #while "" in times:
+    #    times.remove("")
+    #while "" in dist:
+    #    dist.remove("")
+
     print(times)
     print(dist)
-    res = []
-    for i in range(len(times)):
-        res[i] = countWays(int(times[i]),int(dist[i]))
+    res = 1
+    #for i in range(len(times)):
+    res = res*countWays(int(times),int(dist))
 
     print(res)
 
